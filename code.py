180b125d9d6368d771dcff5d33138d1de116c4e4
walabouzouita <101278654+walabouzouita@users.noreply.github.com>
2022-03-23 14:41:23 +0100

    Update code.py

--- code.py
+++ code.py
@@ -1,3 +1,34 @@
 import numpy as np
 import numpy.random as rd
 import matplotlib.pyplot as plt
+
+
+def GibbsSampler(nchain, initialisation, data, param) :
+    #chain[i,] = [alpha, beta1, beta2, sigma]
+    
+    #initialisation
+    chain = np.zeros((nchain + 1, 4))
+    chain[0,:] = initialisation
+    
+    
+    for i in range(nchain):
+      
+     #mise à jour de alpha
+    
+    
+    
+     #mise à jour de Beta1
+      
+      
+      
+     #mise à jour de Beta2
+    
+    
+    
+    
+     #mise à jour de sigma
+  
+  return(chain)
+
+
+
